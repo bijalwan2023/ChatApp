215cf536c3d517337710a9471843cd1ea5b073fc
hp <hp@DESKTOP-5JO2GB8>
2024-03-27 12:35:20 +0530

    some changes

--- ChatAppApplication/src/main/java/com/ChatAppApplication/controller/ChatController.java
+++ ChatAppApplication/src/main/java/com/ChatAppApplication/controller/ChatController.java
@@ -1,37 +1,43 @@
-//package com.ChatAppApplication.controller;
-//
-//import org.springframework.messaging.handler.annotation.MessageMapping;
-//import org.springframework.messaging.handler.annotation.Payload;
-//import org.springframework.messaging.handler.annotation.SendTo;
-//import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.ChatAppApplication.models.ChatMessage;
-//
-//
-//
-//@Controller
-//public class ChatController {
-//	
-//	@MessageMapping("/chat.register")
-//	@SendTo("/topic/public")
-//	public ChatMessage register(@Payload ChatMessage message,SimpMessageHeaderAccessor headerAccessor) {
-//		headerAccessor.getSessionAttributes().put("username", message.getSender());
-//		return message;
-//		
-//	}
-//
-//	@MessageMapping("/chat.send")
-//	@SendTo("/topic/public")
-//	public ChatMessage sendMessage(@RequestBody ChatMessage message,Authentication authentication) throws Exception {
-//		if (authentication != null && authentication.isAuthenticated()) {
-//            return message;
-//        } else {
-//            throw new Exception("User not authenticated");
-//        }
-//
-//	}
-//}
+package com.ChatAppApplication.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ChatAppApplication.models.ChatMessage;
+
+
+
+@Controller
+public class ChatController {
+	
+//	@GetMapping("/index")
+//    public String index() {
+//        return "index"; // This returns the name of the HTML file (without the extension)
+//    }
+	
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload ChatMessage message,SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", message.getSender());
+		return message;
+		
+	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@RequestBody ChatMessage message,Authentication authentication) throws Exception {
+		if (authentication != null && authentication.isAuthenticated()) {
+            return message;
+        } else {
+            throw new Exception("User not authenticated");
+        }
+
+	}
+}
